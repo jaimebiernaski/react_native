769e5bd347cffc6162dd92e572c839c6922281bf
Jaime Biernaski <>
2020-06-09 15:52:36 -0300

    add reducer hook

--- rn_hooks_udemy/blog/src/context/BlogContext.js
+++ rn_hooks_udemy/blog/src/context/BlogContext.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 export const BlogContext = React.createContext();
 
+const initialState = [];
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, initialState);
+
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post#${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
